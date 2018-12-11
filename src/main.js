e6032bca71a68e1c6eafc74d21d4ad18c258187e
bug: Fix de bug do routeService <guiihpr@gmail.com>
2018-12-11 19:20:51 -0200

    Trocando o linter pro standard

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-const a = 3;
-const b = 2;
-const c = 'djkassadaSAOJDSAOJSAODJSAOIJDOKSAJD ASSAIJIOJsdjoijdiojasiodjjoijdaoidsjaoidjiojaiojoidJAoidjAOIsdjOIdsjOJdsoIJ';
+const a = 3
+const b = 2
+const c = 'djkassadaSAOJDSAOJSAODJSAOIJDOKSAJD ASSAIJIOJsdjoijdiojasiodjjoijdaoidsjaoidjiojaiojoidJAoidjAOIsdjOIdsjOJdsoIJ'
 
-function sum() {
-  return a + b + c;
+function sum () {
+  return a + b + c
 }
 
-sum();
+sum()
