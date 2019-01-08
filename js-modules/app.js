a4e89fdb62a71277d8adb039a8e603bdd7f76033
bug: Fix de bug do routeService <guiihpr@gmail.com>
2019-01-08 14:46:42 -0200

    JS modules, modulo do webpack

--- js-modules/app.js
+++ js-modules/app.js
@@ -4,5 +4,7 @@ const arr1 = [1, 1, 1]
 const arr2 = [2, 2, 2]
 
 const arr3 = R.union(arr1, arr2)
+const arr4 = R.uniq(arr1)
 
 console.log(arr3)
+console.log(arr4)
--- src/main.js
+++
@@ -1,9 +0,0 @@
-const a = 3
-const b = 2
-const c = 'djkassadaSAOJDSAOJSAODJSAOIJDOKSAJD ASSAIJIOJsdjoijdiojasiodjjoijdaoidsjaoidjiojaiojoidJAoidjAOIsdjOIdsjOJdsoIJ'
-
-function sum () {
-  return a + b + c
-}
-
-sum()
